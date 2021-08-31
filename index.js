4349f2c40bd9db902538129712638a5ae330fe1d
Matthew Blank <matthew.3lank@gmail.com>
2021-08-30 22:42:33 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) {
+    return 'This one is on me!'
+  } else if (distance > 2500) {
+    return 'No can do.'
+  } else if (distance > 2000) {
+    return 'I will gladly take your thirty bucks.'
+  } 
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  if (city === 'NYC') {
+    return 'Ok, sounds good.'
+  } else {
+    return 'No go.'
+  }
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  if (tip === 'generous') {
+    return "Thank you so much."
+  } else if (tip === 'not as generous') {
+    return "Thank you."
+  } else {
+    return "Bye."
+  }
   // Write your code here!
 }
